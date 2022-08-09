c2a560d32b1f3264ecc98ac2e5526e44bca23a47
TiagoNevoa <tiagonevoa@gmail.com>
2022-08-10 00:44:02 +0100

    Atividade 2 using strings and scanner keyboard

--- src/javabasico/Atividade2.java
+++ src/javabasico/Atividade2.java
@@ -1,4 +1,6 @@
 package javabasico;
+
+import java.util.Scanner;
 /*
  * Atividade 2. Fazer um programa, onde serão definidas três variáveis locais, nome, apelidos e idade, sendo esta última de tipo inteiro. Estas variáveis serão introduzidas por consola. Finalmente mostrará uma mensagem em consola usando as variáveis da seguinte forma: “Olá nome apelidos tens idade anos”.
  */
@@ -6,7 +8,23 @@ package javabasico;
 public class Atividade2 {
 
 	public static void main(String[] args) {
-		System.out.println("Atividade 2");
+		
+		String name;
+		String surname;
+		int age;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Nome?");
+		name = keyboard.nextLine();
+		
+		System.out.println("Sobrenome?");
+		surname = keyboard.nextLine();
+		
+		System.out.println("Idade?");
+		age = keyboard.nextInt();
+		
+		System.out.println("Ola " + name + " " + surname + " tens " + age + " anos.");
 	}
 
 }
