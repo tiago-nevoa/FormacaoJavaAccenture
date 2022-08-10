4df459104440950ec0b26c400420271952e2e173
TiagoNevoa <tiagonevoa@gmail.com>
2022-08-10 22:57:59 +0100

    Atividade3, we need make sure that user insert correct data and keep asking for correct data

---
+++ src/javabasico/Atividade3.java
@@ -0,0 +1,24 @@
+package javabasico;
+
+import java.util.Scanner;
+/*
+ * Atividade 3. Faz uma aplicação que calcule a área de um círculo(pi*R2).
+ * O raio será pedido por teclado (lembra-te de passar de String para double com Double.parseDouble).
+ * Usa a constante PI e o método pow de Math.
+ */
+
+public class Atividade3 {
+
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Introduz um raio:");
+		double raio = keyboard.nextDouble();
+		
+		double area = Math.PI * Math.pow(raio,2);
+		
+		System.out.print("A area do circulo e: " + area);
+	}
+
+}
