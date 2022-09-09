62e928c79972925784f3bc4e3937e74efb89e61f
TiagoNevoa <tiagonevoa@gmail.com>
2022-09-09 08:57:02 +0100

    Atividade17

---
+++ src/praticasjava/Atividade17.java
@@ -0,0 +1,12 @@
+package praticasjava;
+/*
+ * Atividade 17. Programa que considere 6 números e os mostre no ecrã do menor ao maior:
+	• Primeiro mostre-se o menor dos 6.
+	• Depois mostre-se o maior dos 6.
+	• Depois mostrem-se os 6 números ordenados do menor para o maior.
+	• Depois mostrem-se os 6 números ordenados do maior para o menor.
+ */
+
+public class Atividade17 {
+
+}
